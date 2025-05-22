6a2b048cd57fb23a382e2d65810ce3c50b7df4fb
unknown <r4f4santoss@gmail.com>
2025-05-22 13:32:16 -0300

    atualiazção ProductView

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -70,7 +70,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
               d="M12 4.5v15m7.5-7.5h-15" />
           </svg>
-          <span class="text-sm font-semibold">+ Vendidos</span>
+          <span class="text-sm font-semibold"> Vendidos</span>
         </a>
       </nav>
     </aside>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -4,14 +4,48 @@
         <table class="w-full text-sm text-left rtl:text-gray-700 dark:text-gray-300">
           <thead class="text-xs text-white uppercase bg-blue-600 dark:bg-blue-800">
             <tr>
-              <th scope="col" class="px-10 py-5 border-r border-blue-400 dark:border-blue-700 py-5">ID</th>
-              <th scope="col" class="px-10 py-5 border-r border-blue-400 dark:border-blue-700 py-5">Nome</th>
-              <th scope="col" class="px-10 py-5 border-r border-blue-400 dark:border-blue-700 py-5">Preço</th>
-              <th scope="col" class="px-10 py-5 border-r border-blue-400 dark:border-blue-700 py-5">Categoria</th>
-              <th scope="col" class="px-10 py-5 border-r border-blue-400 dark:border-blue-700 py-5">Quantidade Venda</th>
+              <th scope="col" class="px-10 py-5 border-r border-b border-blue-400 dark:border-blue-700 py-5">ID</th>
+              <th scope="col" class="px-10 py-5 border-r border-b border-blue-400 dark:border-blue-700 py-5">Nome</th>
+              <th scope="col" class="px-10 py-5 border-r border-b border-blue-400 dark:border-blue-700 py-5">Preço</th>
+              <th scope="col" class="px-10 py-5 border-r border-b border-blue-400 dark:border-blue-700 py-5">Categoria</th>
+              <th scope="col" class="px-10 py-5 border-r border-b border-blue-400 dark:border-blue-700 py-5">Quantidade Venda</th>
             </tr>
           </thead>
+          <tbody>
+            <tr v-for="p in produtos" :key="p.id">
+              <td class="px-10 py-5 border-r border-b border-blue-400 dark:border-blue-700 py-5">1</td>
+              <td class="px-10 py-5 border-r border-b border-blue-400 dark:border-blue-700 py-5">air fryer</td>
+              <td class="px-10 py-5 border-r border-b border-blue-400 dark:border-blue-700 py-5">299.00</td>
+              <td class="px-10 py-5 border-r border-b border-blue-400 dark:border-blue-700 py-5">eletrodomesticos</td>
+              <td class="px-10 py-5 border-r border-b border-blue-400 dark:border-blue-700 py-5">100</td>
+            </tr>
+          </tbody>
         </table>
     </main>
   </div>
+
+  
+  
 </template>
+
+<script setup>
+
+import { ref, onMounted } from "vue"
+import axios from 'axios'
+
+const produtos = ref([]) 
+const offset = ref(0) 
+const limit = 20
+
+const fetchProduto = async function () => {
+  const response = await axios.get(`https://localhost:7256/v1/produtos`)
+  const results = response.data
+  console.log(results);
+  
+}
+
+
+
+onMounted(fetchProduto)
+</script>
+
